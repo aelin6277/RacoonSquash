be42783631b03fa12dd0749b2e1af8a92d87c25e
jhaard <jorgen.hard@gmail.com>
2023-11-30 14:58:55 +0100

    -Edited bounds to both games with Path-class.

--- app/src/main/java/com/example/racoonsquash/PongActivity.kt
+++ app/src/main/java/com/example/racoonsquash/PongActivity.kt
@@ -3,8 +3,11 @@ package com.example.racoonsquash
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.PathMeasure
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
@@ -12,6 +15,12 @@ class PongActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
     private lateinit var surfaceView: SurfaceView
     private lateinit var lineColor: Paint
+
+    // Path-klass ritar ett "spår" från en punkt moveTo() till nästa punkt lineTo()
+    // Dessa blir gränser för spel-plan
+    // Verkar kunna använda PathMeasure-klassen för att detektera intersections/collisions
+    private var leftBoundaryPath: Path? = null
+    private var rightBoundaryPath: Path? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +31,20 @@ class PongActivity : AppCompatActivity(), SurfaceHolder.Callback {
         // Enbart för att synliggöra gränserna
         lineColor = Paint().apply {
             color = Color.CYAN
-            strokeWidth = 20f
+            style = Paint.Style.STROKE
+            strokeWidth = 10f
         }
         surfaceView.holder.addCallback(this)
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
-        gameBounds(holder)
+
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-
+        leftBoundaryPath = createBoundaryPathLeft(width, height)
+        rightBoundaryPath = createBoundaryPathRight(width, height)
+        gameBounds(holder)
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
@@ -41,12 +53,36 @@ class PongActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
     fun gameBounds(holder: SurfaceHolder) {
         val canvas: Canvas? = holder.lockCanvas()
-        val pointViewWidth = surfaceView.width.toFloat()
-        val pointViewHeight = surfaceView.height.toFloat()
 
-        canvas?.drawLine(pointViewWidth, 0f, pointViewWidth, pointViewHeight, lineColor)
-        canvas?.drawLine(0f, 0f, 0f, pointViewHeight, lineColor)
+        // canvas?.drawColor(Color.BLACK)
+
+        rightBoundaryPath?.let {
+            canvas?.drawPath(it, lineColor)
+        }
+
+        leftBoundaryPath?.let {
+            canvas?.drawPath(it, lineColor)
+        }
 
         holder.unlockCanvasAndPost(canvas)
     }
+
+    private fun createBoundaryPathLeft(width: Int, height: Int): Path {
+        val pathLeft = Path()
+
+        pathLeft.moveTo(0f, 0f)
+        pathLeft.lineTo(0f, height.toFloat())
+
+        return pathLeft
+    }
+
+    private fun createBoundaryPathRight(width: Int, height: Int): Path {
+        val pathRight = Path()
+
+        pathRight.moveTo(width.toFloat(), 0f)
+        pathRight.lineTo(width.toFloat(), height.toFloat())
+
+        return pathRight
+    }
+
 }
--- app/src/main/java/com/example/racoonsquash/SquashActivity.kt
+++ app/src/main/java/com/example/racoonsquash/SquashActivity.kt
@@ -3,6 +3,7 @@ package com.example.racoonsquash
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.SurfaceHolder
@@ -13,6 +14,11 @@ class SquashActivity : AppCompatActivity(), SurfaceHolder.Callback {
     private lateinit var surfaceView: SurfaceView
     private lateinit var lineColor: Paint
 
+    //Path-klass ritar ett "spår" från en punkt moveTo() till nästa punkt lineTo()
+    // Dessa blir gränser för spel-plan
+    // Verkar kunna använda PathMeasure-klassen för att detektera intersections/collisions
+    private var gameBoundaryPath: Path? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_squash)
@@ -22,36 +28,46 @@ class SquashActivity : AppCompatActivity(), SurfaceHolder.Callback {
         // Enbart för att synliggöra gränserna
         lineColor = Paint().apply {
             color = Color.MAGENTA
-            strokeWidth = 20f
+            style = Paint.Style.STROKE
+            strokeWidth = 10f
         }
 
         surfaceView.holder.addCallback(this)
 
     }
+
     override fun surfaceCreated(holder: SurfaceHolder) {
-        gameBounds(holder)
 
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-
+        gameBoundaryPath = createBoundaryPath(width, height)
+        drawGameBounds(holder)
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
 
     }
 
-    fun gameBounds(holder: SurfaceHolder) {
+    fun drawGameBounds(holder: SurfaceHolder) {
         val canvas: Canvas? = holder.lockCanvas()
-        val pointViewWidth = surfaceView.width.toFloat()
-        val pointViewHeight = surfaceView.height.toFloat()
 
+        // canvas?.drawColor(Color.BLACK)
 
-        canvas?.drawLine(0f, 0f, pointViewWidth, 0f, lineColor)
-        canvas?.drawLine(pointViewWidth, 0f, pointViewWidth, pointViewHeight, lineColor)
-        canvas?.drawLine(0f, pointViewHeight, pointViewWidth, pointViewHeight, lineColor)
+        gameBoundaryPath?.let {
+            canvas?.drawPath(it, lineColor)
+        }
 
         holder.unlockCanvasAndPost(canvas)
     }
 
+    private fun createBoundaryPath(width: Int, height: Int): Path {
+        val path = Path()
+        path.moveTo(0f, 0f)
+        path.lineTo(width.toFloat(), 0f)
+        path.lineTo(width.toFloat(), height.toFloat())
+        path.lineTo(0f, height.toFloat())
+
+        return path
+    }
 }
