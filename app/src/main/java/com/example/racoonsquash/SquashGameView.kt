4e0105cc6bf098fabbaa8e62d25294cd5c757f0d
jhaard <jorgen.hard@gmail.com>
2023-12-01 09:13:03 +0100

    -Integrated background to squash-pong.

--- app/src/main/java/com/example/racoonsquash/SquashGameView.kt
+++ app/src/main/java/com/example/racoonsquash/SquashGameView.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.Rect
 import android.icu.text.Transliterator
 import android.view.MotionEvent
@@ -17,6 +18,12 @@ class SquashGameView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
     lateinit var ball1: Ball
     lateinit var posX: Transliterator.Position
     lateinit var posY: Transliterator.Position
+    private var lineColor: Paint
+
+    //Path-klass ritar ett "spår" från en punkt moveTo() till nästa punkt lineTo()
+    // Dessa blir gränser för spel-plan
+    // Verkar kunna använda PathMeasure-klassen för att detektera intersections/collisions
+    private var gameBoundaryPath: Path? = null
 
     var bounds = Rect() //for att kunna studsa m vaggarna
     var mHolder: SurfaceHolder? = holder
@@ -24,6 +31,13 @@ class SquashGameView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
     init {
         if (mHolder != null) {
             mHolder?.addCallback(this)
+        }
+
+        // Enbart för att synliggöra gränserna
+        lineColor = Paint().apply {
+            color = Color.MAGENTA
+            style = Paint.Style.STROKE
+            strokeWidth = 10f
         }
         setup()
     }
@@ -37,7 +51,6 @@ class SquashGameView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
 //        ball1.posY = 100f
 //        ball1.paint.color = Color.RED
     }
-
     fun start() {
         running = true
         thread =
@@ -67,21 +80,19 @@ class SquashGameView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
 
         return true
         // return super.onTouchEvent(event)
-
     }
 
-
-    fun draw() {
-        canvas = holder!!.lockCanvas()
-        canvas.drawColor(Color.BLUE)
-        ball1.draw(canvas)
-
-        holder!!.unlockCanvasAndPost(canvas)
-    }
+//    fun draw() {
+//        canvas = holder!!.lockCanvas()
+//        canvas.drawColor(Color.BLUE)
+//
+//
+//        holder!!.unlockCanvasAndPost(canvas)
+//    }
 
     //dessa startar och stoppar min thread:
     override fun surfaceCreated(holder: SurfaceHolder) {
-       // start()
+        // start()
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -98,9 +109,28 @@ class SquashGameView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
     override fun run() {
         while (running) {
             update()
-            draw()
+            drawGameBounds(holder)
+            gameBoundaryPath = createBoundaryPath(width, height)
             ball1.checkBounds(bounds)
         }
     }
+    fun drawGameBounds(holder: SurfaceHolder) {
+        val canvas: Canvas? = holder.lockCanvas()
+        canvas?.drawColor(Color.BLACK)
 
+        gameBoundaryPath?.let {
+            canvas?.drawPath(it, lineColor)
+        }
+        ball1.draw(canvas)
+        holder.unlockCanvasAndPost(canvas)
+    }
+
+    private fun createBoundaryPath(width: Int, height: Int): Path {
+        val path = Path()
+        path.moveTo(0f, 0f)
+        path.lineTo(width.toFloat(), 0f)
+        path.lineTo(width.toFloat(), height.toFloat())
+        path.lineTo(0f, height.toFloat())
+        return path
+    }
 }
